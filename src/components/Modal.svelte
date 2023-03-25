419ef333b421a37d4429b402338649d90dbf574e
Jhosimar Aguacia <corbin026@hotmail.com>
2023-03-25 14:01:03 +0200

    mend

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -1,11 +1,17 @@
 <script>
+  // stopPropagation - stop the event bubbling or capturing any further
+  // capture - fires the handler during the capture phase not bubbling
+  // once - makes sure the event can only fire once (removes handler)
+  // preventDefault - prevent the default action (run e.preventDefault())
+  // self - only fires the event if the clicked element is the target
+
   export let msg = 'Sign up for more!';
   export let showModal = false;
   export let isPromo = false;
 </script>
 
 {#if showModal}
-  <div class="backdrop" class:promo={isPromo} on:click>
+  <div class="backdrop" class:promo={isPromo} on:click|self>
     <div class="modal">
       <p>{msg}</p>
     </div>
