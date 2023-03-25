f16dcec3d5c5b3638660fccae441702a5e0eae9b
Jhosimar Aguacia <corbin026@hotmail.com>
2023-03-25 09:38:07 +0200

    Add two way binding

--- src/App.svelte
+++ src/App.svelte
@@ -5,12 +5,21 @@
 	const updateBeltColour = () => {
 		beltColour = 'red';
 	};
+
+	// I could have used this function and call it from the main by adding the following line of code
+	// <input on:input={handleInput} type="text" value={beltColour}>. However, using bind:value is a shorter way of 
+	// doing the same thing.
+	// const handleInput = (e) => {
+	// 	beltColour = e.target.value;
+	// };
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Hello, Ninja with {beltColour} belt</p>
+	<p style="color: {beltColour}">Hello, Ninja with {beltColour} belt</p>
 	<button on:click={updateBeltColour}>update belt color</button>
+	<!-- <input on:input={handleInput} type="text" value={beltColour}> -->
+	<input type="text" bind:value={beltColour}>
 </main>
 
 <style>
