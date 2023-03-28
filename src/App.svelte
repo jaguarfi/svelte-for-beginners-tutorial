37130ee78a5cb3b21675ea562632cfa2c5ad711e
Jhosimar Aguacia <corbin026@hotmail.com>
2023-03-28 19:53:00 +0300

    Add footer and header to the app

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,17 @@
 <script>
-
+  import Footer from './components/Footer.svelte';
+  import Header from './components/Header.svelte';
 </script>
 
-
+<Header />
 <main>
-<h1>Start here!</h1>
+  <h1>Start here!</h1>
 </main>
+<Footer />
 
 <style>
-
+  main {
+    max-width: 960px;
+    margin: 40px auto;
+  }
 </style>
