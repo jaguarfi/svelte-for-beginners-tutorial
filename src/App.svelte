959dfcc45225c3d6de22d0e23912049c714097a5
Jhosimar Aguacia <corbin026@hotmail.com>
2023-03-28 21:03:17 +0300

    Add AddPollForm component

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,13 @@
   import Footer from './components/Footer.svelte';
   import Header from './components/Header.svelte';
   import Tabs from './shared/Tabs.svelte';
+  import AddPollForm from './components/AddPollForm.svelte';
 
   let tabs = ['Current Polls', 'Add Poll'];
   let activeItem = tabs[0];
 
   const handleChangeTab = (e) => {
-	activeItem = e.detail;
+    activeItem = e.detail;
   };
 </script>
 
@@ -16,11 +17,10 @@
   <h1>Start here!</h1>
   <Tabs on:changeTab={handleChangeTab} {tabs} {activeItem} />
   {#if tabs[0] === activeItem}
-	<!-- <CurrentPolls /> -->
-	<p>Current Polls</p>
+    <!-- <CurrentPolls /> -->
+    <p>Current Polls</p>
   {:else if tabs[1] === activeItem}
-	<!-- <AddPoll /> -->
-	<p>Add Poll</p>
+    <AddPollForm />
   {/if}
 </main>
 <Footer />
---
+++ src/components/AddPollForm.svelte
@@ -0,0 +1,53 @@
+<script>
+  import {createEventDispatcher} from 'svelte';
+
+  const dispatch = createEventDispatcher();
+  let fields = {
+    property: '',
+    answerA: '',
+    answerB: ''
+  };
+
+  const handleSubmit = () => {
+    dispatch('addPoll', fields);
+  };
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <div class="form-field">
+    <label for="question">Poll Question:</label>
+    <input type="text" id="question" bind:value={fields.question} />
+  </div>
+  <div class="form-field">
+    <label for="answer-a">Answer A:</label>
+    <input type="text" id="answer-a" bind:value={fields.answerA} />
+  </div>
+  <div class="form-field">
+    <label for="answer-b">Answer B:</label>
+    <input type="text" id="answer-b" bind:value={fields.answerB} />
+  </div>
+  <button>Add Poll</button>
+</form>
+
+<style>
+  form {
+    max-width: 400px;
+    margin: 0 auto;
+    text-align: center;
+  }
+  .form-field {
+    margin: 18px auto;
+  }
+  input {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 6px;
+  }
+
+  label {
+    margin: 10px auto;
+    text-align: left;
+    margin-bottom: 10px;
+  }
+</style>
