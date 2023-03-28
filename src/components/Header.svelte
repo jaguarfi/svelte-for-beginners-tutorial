8acd4df4014df99ec6974fe2a88af24dc6e2d880
Jhosimar Aguacia <corbin026@hotmail.com>
2023-03-28 19:52:01 +0300

    Add Header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,21 @@
+<script></script>
+<header>
+    <h1>
+        <img src="/images/poll-icon.png" alt="Poll Logo" srcset="">
+    </h1>
+</header>
+<style>
+    header {
+        background-color: #f7f7f7;
+        padding: 20px;
+    }
+
+    h1 {
+        margin: 0;
+        text-align: center;
+    }
+
+    img {
+        max-width: 180px;
+    }
+</style>
